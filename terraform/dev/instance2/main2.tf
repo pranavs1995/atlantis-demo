083d14a4fcedb44486c094393131ec418a8a4b7f
Pranav Shankar <pranav.shankar@solarwinds.com>
2024-07-09 21:13:22 +0530

    update all envs

--- terraform/dev/instance2/main2.tf
+++ terraform/dev/instance2/main2.tf
@@ -7,6 +7,7 @@ provider "aws" {
 resource "aws_instance" "example2" {
   ami           = "ami-09b90e09742640522"
   instance_type = "t2.micro"
+
   tags = {
     Name = "demo_instance_dev2"
     managed_by = "atlantis"
