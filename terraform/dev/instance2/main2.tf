b544fb394454ae7a2de72e1d16ce29b0a76bb980
Pranav Shankar <pranav.shankar@solarwinds.com>
2024-07-15 22:46:36 +0530

    testing update

--- terraform/dev/instance2/main2.tf
+++ terraform/dev/instance2/main2.tf
@@ -4,11 +4,11 @@ provider "aws" {
 }
 
 # Create an EC2 instance
-# resource "aws_instance" "example2" {
-#   ami           = "ami-0b72821e2f351e396"
-#   instance_type = "t2.micro"
-#   tags = {
-#     Name = "demo_instance_dev2"
-#     managed_by = "atlantis"
-#   }
-# }
+resource "aws_instance" "example2" {
+  ami           = "ami-0b72821e2f351e396"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "demo_instance_dev3"
+    managed_by = "atlantis"
+  }
+}
